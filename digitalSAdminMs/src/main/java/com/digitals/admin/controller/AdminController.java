2b6df3b593cbc86e2fe9e6cd99c3a1bbdbd4a94a
Dharmapuri_V <Dharmapuri_V@ENGLON108694L>
2018-06-10 20:26:49 +0100

    admin modoule ms changes committed

---
+++ digitalSAdminMs/src/main/java/com/digitals/admin/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.digitals.admin.controller;
+
+import com.digitals.admin.request.entities.UserDetails;
+
+public class AdminController {
+
+	
+	public String registerUser(UserDetails userDetails) {
+		
+		return null;
+		
+	}
+}
---
+++ digitalSAdminMs/src/main/java/com/digitals/admin/request/entities/UserDetails.java
@@ -0,0 +1,68 @@
+package com.digitals.admin.request.entities;
+
+import java.util.Date;
+import java.util.Map;
+
+public class UserDetails {
+	
+	private String firstName;
+	private String lastName;
+	private Date dob;
+	private String email;
+	private String username;
+	private String password;
+	private String role;
+	private Map<String,String> securityQuestions;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Map<String, String> getSecurityQuestions() {
+		return securityQuestions;
+	}
+	public void setSecurityQuestions(Map<String, String> securityQuestions) {
+		this.securityQuestions = securityQuestions;
+	}
+	
+
+	
+
+}
