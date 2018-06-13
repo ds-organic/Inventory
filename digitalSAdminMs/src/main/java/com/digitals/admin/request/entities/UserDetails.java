921ac2c46e497080302ad4849bae5b14053c6678
Dharmapuri_V <Dharmapuri_V@ENGLON108694L>
2018-06-13 22:59:41 +0100

    User details committed

--- digitalSAdminMs/src/main/java/com/digitals/admin/request/entities/UserDetails.java
+++ digitalSAdminMs/src/main/java/com/digitals/admin/request/entities/UserDetails.java
@@ -11,6 +11,12 @@ public class UserDetails {
 	private String email;
 	private String username;
 	private String password;
+	private String postcode;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String country;
+	private String contactNumber;
 	private String role;
 	private Map<String,String> securityQuestions;
 	public String getFirstName() {
@@ -60,6 +66,42 @@ public class UserDetails {
 	}
 	public void setSecurityQuestions(Map<String, String> securityQuestions) {
 		this.securityQuestions = securityQuestions;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
 	}
 	
 
